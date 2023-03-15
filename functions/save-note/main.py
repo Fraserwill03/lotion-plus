61616de8fe45f71083ec3b4b2e2cba5105fef3cc
AarshShah9 <aarshshah99@gmail.com>
2023-03-15 11:40:46 -0600

    Added template terraform code

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,1 +1,26 @@
+import json
+import boto3
+
 # add your save-note function here
+
+
+def save_note(event, context):
+    try:
+
+        # create a dynamodb client
+        dynamodb = boto3.client('dynamodb')
+        # insert the note object into the database
+        dynamodb.put_item(
+            TableName='Notes',
+            Item=event['body']
+        )
+        # return a success message
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Note saved successfully!')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error saving note: ' + str(e))
+        }
